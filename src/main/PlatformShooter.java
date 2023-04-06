c61c526429c322a66cf24929b210d50826ad2876
garthendrich <garthendrich@gmail.com>
2023-04-06 20:12:40 +0800

    feat: create window

--- src/main/PlatformShooter.java
+++ src/main/PlatformShooter.java
@@ -1,7 +1,20 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class PlatformShooter {
     public static void main(String[] args) {
-        System.out.println("piw piw");
+        JFrame window = createWindow();
+        window.setVisible(true);
+    }
+
+    private static JFrame createWindow() {
+        JFrame window = new JFrame();
+        window.setSize(960, 540);
+        window.setTitle("");
+        window.setResizable(false);
+        window.setLocationRelativeTo(null); // center window on screen
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return window;
     }
 }
