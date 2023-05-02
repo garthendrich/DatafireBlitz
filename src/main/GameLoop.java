9e2aec1b79d7d2db70e7250dc457be2b5a45b889
Paulo Rodriguez <48713934+pirodriguez@users.noreply.github.com>
2023-05-02 17:24:12 +0800

    Revert "Added bullet and down movement"

--- src/main/GameLoop.java
+++ src/main/GameLoop.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import components.Entity;
 import components.Player;
-import components.Bullet;
 
 public class GameLoop implements Runnable {
     private int FPS = 120;
@@ -19,7 +18,6 @@ public class GameLoop implements Runnable {
     private Player dummy2 = new Player(300, 0);
     private Player dummy3 = new Player(500, 0);
     private ArrayList<Entity> platforms = new ArrayList<Entity>();
-    private ArrayList<Bullet> bullets = new ArrayList<Bullet>(); 
 
     GameLoop(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
@@ -34,21 +32,6 @@ public class GameLoop implements Runnable {
         platforms.add(new Entity(128, gamePanel.getHeight() - 96, gamePanel.getWidth() - 256, 32));
     }
 
-    private void createBullet(){
-        if(keyInputs.bulletLeft || keyInputs.bulletRight){
-
-            Bullet b = new Bullet(player.getPosX(), player.getPosY());
-            if(keyInputs.bulletLeft){
-                b.moveLeft();
-            }else if(keyInputs.bulletRight){
-                b.moveRight();
-            }
-
-            bullets.add(b);
-
-        }
-    }
-
     void start() {
         gameThread.start();
         gamePanel.addKeyListener(keyInputs);
@@ -56,26 +39,16 @@ public class GameLoop implements Runnable {
     }
 
     @Override
-    public void run(){
+    public void run() {
         double currentTime = System.nanoTime();
         double nextFrameNanoSeconds = currentTime + NANO_SECONDS_PER_FRAME;
-
-        int timer = 20;
 
         while (true) {
             if (currentTime >= nextFrameNanoSeconds) {
                 updatePlayerMovement();
-
-                if (timer == 0){
-                    createBullet();
-                    timer = 20;
-                }
-
-                updateBulletMovement();
                 updateCanvas();
                 updateDummyMovement();
 
-                timer--;
                 nextFrameNanoSeconds += NANO_SECONDS_PER_FRAME;
             }
 
@@ -95,7 +68,6 @@ public class GameLoop implements Runnable {
 
     private void updatePlayerMovement() {
         player.stopHorizontalMovement();
-
         if (keyInputs.moveLeft && !keyInputs.moveRight) {
             player.moveLeft();
         } else if (keyInputs.moveRight && !keyInputs.moveLeft) {
@@ -104,8 +76,6 @@ public class GameLoop implements Runnable {
 
         if (keyInputs.moveUp) {
             player.jump();
-        } else if(keyInputs.moveDown){
-            player.moveDown();
         }
 
         if (player.getPosY() >= 700){
@@ -116,17 +86,10 @@ public class GameLoop implements Runnable {
         player.updatePosition(platforms);
     }
 
-    private void updateBulletMovement(){
-        for (Bullet b: bullets){
-            b.updatePosition();
-        }
-    }
-
     private void updateCanvas() {
         ArrayList<Entity> entities = new ArrayList<Entity>();
         entities.add(player);
         entities.addAll(platforms);
-        entities.addAll(bullets);
         entities.add(dummy1);
         entities.add(dummy2);
         entities.add(dummy3);
