ddc53b9b4bcace9ad584ace449300384ed4f7543
pirodriguez <pirodriguez@up.edu.ph>
2023-05-02 14:51:28 +0800

    feat- added Dummies in the field and respawn for players & dummies

--- src/main/GameLoop.java
+++ src/main/GameLoop.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import components.Entity;
 import components.Player;
+import components.Dummy;
 
 public class GameLoop implements Runnable {
     private int FPS = 120;
@@ -14,6 +15,9 @@ public class GameLoop implements Runnable {
     private KeyInputs keyInputs = new KeyInputs();
 
     private Player player = new Player(400, 0);
+    private Dummy dummy1 = new Dummy(200, 0);
+    private Dummy dummy2 = new Dummy(300, 0);
+    private Dummy dummy3 = new Dummy(500, 0);
     private ArrayList<Entity> platforms = new ArrayList<Entity>();
 
     GameLoop(GamePanel gamePanel) {
@@ -44,12 +48,23 @@ public class GameLoop implements Runnable {
             if (currentTime >= nextFrameNanoSeconds) {
                 updatePlayerMovement();
                 updateCanvas();
+                updateDummyMovement();
 
                 nextFrameNanoSeconds += NANO_SECONDS_PER_FRAME;
             }
 
             currentTime = System.nanoTime();
         }
+    }
+
+    private void updateDummyMovement(){
+        dummy1.updatePosition(platforms);
+        dummy2.updatePosition(platforms);
+        dummy3.updatePosition(platforms);
+
+        if(dummy1.getPosY() >= 700) dummy1.respawn();
+        if(dummy2.getPosY() >= 700) dummy2.respawn();
+        if(dummy3.getPosY() >= 700) dummy3.respawn();
     }
 
     private void updatePlayerMovement() {
@@ -64,6 +79,11 @@ public class GameLoop implements Runnable {
             player.jump();
         }
 
+        if (player.getPosY() >= 700){
+            player.respawn();
+            
+        }
+
         player.updatePosition(platforms);
     }
 
@@ -71,6 +91,9 @@ public class GameLoop implements Runnable {
         ArrayList<Entity> entities = new ArrayList<Entity>();
         entities.add(player);
         entities.addAll(platforms);
+        entities.add(dummy1);
+        entities.add(dummy2);
+        entities.add(dummy3);
 
         gamePanel.setEntities(entities);
         gamePanel.repaint();
