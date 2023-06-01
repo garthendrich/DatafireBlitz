f1bd5fc5b7b1d12216ddeb05b0de520d99e80fbe
garthendrich <garthendrich@gmail.com>
2023-06-01 16:35:25 +0800

    feat: Rename main class

--- src/main/DatafireBlitz.java
+++ src/main/DatafireBlitz.java
@@ -1,7 +1,6 @@
 package main;
 
-// ! temporary class name
-public class PlatformShooter {
+public class DatafireBlitz {
     public static void main(String[] args) {
         Lobby lobby = new Lobby();
         lobby.homepage();
